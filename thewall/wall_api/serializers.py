7abc82eaf9024b6c9aab2550d3f09b3e869d1c52
Alison Reilly <reillyamr@gmail.com>
2020-09-23 11:26:37 -0400

    define and add messages to user serializer

--- thewall/wall_api/serializers.py
+++ thewall/wall_api/serializers.py
@@ -2,10 +2,12 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Message
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    messages = serializers.PrimaryKeyRelatedField(many=True, queryset=Message.objects.all())
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'messages']
+        fields = ['id', 'username', 'email', 'messages']
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
