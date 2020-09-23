ac90525ab80a0e1eee4ac9de6673436eafda55f1
Alison Reilly <reillyamr@gmail.com>
2020-09-23 11:27:11 -0400

    add view for User

--- thewall/wall_api/views.py
+++ thewall/wall_api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    CreateAPIView,
+    DestroyAPIView,
+    UpdateAPIView
+)
 
-# Create your views here.
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from .models import Message
+
+class UserList(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
