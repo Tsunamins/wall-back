5b69546659350247d2caec19a2d11a2ec5b67498
Alison Reilly <reillyamr@gmail.com>
2020-09-23 12:44:18 -0400

    add null and blank params to message model

--- thewall/wall_api/models.py
+++ thewall/wall_api/models.py
@@ -6,7 +6,8 @@ from django.contrib.auth.models import User
 class Message(models.Model):
     content = models.CharField(max_length=250)
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    user = models.ForeignKey('auth.User', related_name='messages', on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', related_name='messages', on_delete=models.CASCADE, null=True, blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
