9e65286f35a9ec151e2e7fd09c7c74498c226d8a
Alison Reilly <reillyamr@gmail.com>
2020-09-23 12:44:44 -0400

    add messages list and create views

--- thewall/wall_api/urls.py
+++ thewall/wall_api/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
 
 from .views import (
+    UserList,
+    UserDetail,
     MessageListView,
     MessageDetailView,
     MessageCreateView,
     MessageUpdateView,
     MessageDeleteView,
-    UserList,
-    UserDetail
+  
 )
 
 urlpatterns = [
  path('users/', UserList.as_view()),
- path('users/<int:pk>/', UserDetail.as_view())
+ path('users/<int:pk>/', UserDetail.as_view()),
+ path('messages/', MessageListView.as_view()),
+ path('messages/<int:pk>/', MessageDetailView.as_view()),
+ path('messages/create/', MessageCreateView.as_view()),
 ]
