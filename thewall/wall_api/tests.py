d6e70e545a129fc2e44326e84e6c8b24709b8c76
Alison Reilly <reillyamr@gmail.com>
2020-09-23 14:54:30 -0400

    add model test

--- thewall/wall_api/tests.py
+++ thewall/wall_api/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Message, User
 
-# Create your tests here.
+
+class ModelTestCase(TestCase):
+
+    def setUp(self):
+        self.newuser = User.objects.create(username='newuser')
+        self.content = 'Message from the tests!!!'
+        self.message = Message(
+            content=self.content, user=self.newuser)
+
+    def test_model_can_create_a_message(self):
+        """Test the bucketlist model can create a bucketlist."""
+        old_count = Message.objects.count()
+        self.message.save()
+        new_count = Message.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
