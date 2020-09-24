303d37b14e88e558534a72460f11c45fa507a132
Alison Reilly <reillyamr@gmail.com>
2020-09-24 17:11:12 -0400

    add addtl urls

--- thewall/wall_api/urls.py
+++ thewall/wall_api/urls.py
@@ -4,10 +4,11 @@ from .views import (
     UserList,
     UserDetail,
     MessageListView,
-    MessageDetailView,
+   # MessageDetailView,
     MessageCreateView,
-    MessageUpdateView,
-    MessageDeleteView,
+   # MessageUpdateView,
+   # MessageDeleteView,
+    MessageRetrieveUpdateDestroyView
   
 )
 
@@ -15,6 +16,8 @@ urlpatterns = [
  path('users/', UserList.as_view()),
  path('users/<int:pk>/', UserDetail.as_view()),
  path('messages/', MessageListView.as_view()),
- path('messages/<int:pk>/', MessageDetailView.as_view()),
+ path('messages/<int:pk>/', MessageRetrieveUpdateDestroyView.as_view()),
  path('messages/create/', MessageCreateView.as_view()),
+ path('messages/<int:pk>/update/', MessageRetrieveUpdateDestroyView.as_view()),
+ path('messages/<int:pk>/delete/', MessageRetrieveUpdateDestroyView.as_view()),
 ]
