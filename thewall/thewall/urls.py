78c43064cb2fbc833bdb4366e30d57122af62407
Alison Reilly <reillyamr@gmail.com>
2020-09-24 17:10:29 -0400

    remove extra lines

--- thewall/thewall/urls.py
+++ thewall/thewall/urls.py
@@ -6,7 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('non-admin/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    # path('users/', views.UserList.as_view()),
-    # path('users/<int:pk>/', views.UserDetail.as_view()),
     path('wall-api/', include('wall_api.urls'))
 ]
