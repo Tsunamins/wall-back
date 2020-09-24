dacc7c1be8fab885f83ca210bc5d561f227ce780
Alison Reilly <reillyamr@gmail.com>
2020-09-24 15:51:51 -0400

    add test for create endpoint

--- thewall/wall_api/tests.py
+++ thewall/wall_api/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from .models import Message, User
 
+from rest_framework.test import APIClient
+from rest_framework import status
+from rest_framework.test import force_authenticate
+
 
 class ModelTestCase(TestCase):
 
@@ -11,9 +15,26 @@ class ModelTestCase(TestCase):
             content=self.content, user=self.newuser)
 
     def test_model_can_create_a_message(self):
-        """Test the bucketlist model can create a bucketlist."""
+        """Test if message model creates a message."""
         old_count = Message.objects.count()
         self.message.save()
         new_count = Message.objects.count()
         self.assertNotEqual(old_count, new_count)
 
+
+class ViewTestCase(TestCase):
+    """Test suite for the api views."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        user = User.objects.create(username='apitestuser')
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+
+        self.message_data = {'content': 'New message from tests', 'user': user.id}
+        self.response = self.client.post('/wall-api/messages/create/', self.message_data, format='json')
+      
+
+    def test_api_can_create_a_message(self):
+        """Test if api has message creation capability."""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
