1b8b07a01504f477306af73a2bbda77b12bc721a
Alison Reilly <reillyamr@gmail.com>
2020-09-25 11:28:08 -0400

    add urls for rest-auth

--- thewall/thewall/urls.py
+++ thewall/thewall/urls.py
@@ -6,5 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('non-admin/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('wall-api/', include('wall_api.urls'))
 ]
