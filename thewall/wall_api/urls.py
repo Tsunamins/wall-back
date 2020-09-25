b8307e812e94439fa356f77a11806e4b21c3baa5
Alison Reilly <reillyamr@gmail.com>
2020-09-25 15:32:45 -0400

    add urls for email/confirmation

--- thewall/wall_api/urls.py
+++ thewall/wall_api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from allauth.account.views import ConfirmEmailView
+from . import views
+
 from .views import (
     UserList,
     UserDetail,
@@ -20,4 +23,9 @@ urlpatterns = [
  path('messages/create/', MessageCreateView.as_view()),
  path('messages/<int:pk>/update/', MessageRetrieveUpdateDestroyView.as_view()),
  path('messages/<int:pk>/delete/', MessageRetrieveUpdateDestroyView.as_view()),
+ 
+ path('registration/account-email-verification-sent/', views.null_view, name='account_email_verification_sent'),
+ path('registration/account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
+ path('registration/complete/$', views.complete_view, name='account_confirm_complete'),
+ path('password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.null_view, name='password_reset_confirm'),
 ]
