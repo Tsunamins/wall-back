fcdb12ee645a474ffe74ab42084c5583a62cac86
Alison Reilly <reillyamr@gmail.com>
2020-10-03 14:47:30 -0400

    import post_save, send_mail & add def welcome_user

--- thewall/wall_api/models.py
+++ thewall/wall_api/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.core.mail import send_mail
 
-# Create your models here.
+
+def welcome_user(sender, instance, **kwargs):
+    if kwargs["created"]:
+        new_user_email = instance.email
+        new_username = instance.username
+        url = 'http://127.0.0.1:8000/rest-auth/login/'
+        subject = 'Welcome to the Wall'
+        message = 'Welcome to the Wall ' + new_username + ', \n' + 'Log in with the link below to start the Writing on the Wall. \n' + url
+        send_mail(subject, message, from_email=None, recipient_list=[new_user_email], fail_silently=False, html_message=None)
+        print(new_user_email + " from welcome user function")
+
+
+post_save.connect(welcome_user, sender=User)
+
 
 class Message(models.Model):
     content = models.CharField(max_length=250)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # user = models.ForeignKey('auth.User', related_name='messages', on_delete=models.CASCADE, null=False, blank=False)
     user = models.ForeignKey('auth.User', related_name='messages', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
