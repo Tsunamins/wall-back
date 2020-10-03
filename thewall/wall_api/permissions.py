c37c4a81abf0d77a92a14dbd7924368cbcd1ab61
Alison Reilly <reillyamr@gmail.com>
2020-10-03 19:22:04 -0400

    update notes

--- thewall/wall_api/permissions.py
+++ thewall/wall_api/permissions.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 class UserLoggedInandMatch(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        # can write custom code
+        # kept prints for note to self purposes
         print('shows article id number:')
-        print(view.kwargs) # shows article id number
+        print(view.kwargs)
         print('shows current user/logged in')
-        print(request.user) # shows current username
+        print(request.user)
         print('shows user of message')
         print(obj.user)
         print(request.user == obj.user)
