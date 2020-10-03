7c529dbd2ba9fb95150fadb124659aa06cc574a2
Alison Reilly <reillyamr@gmail.com>
2020-10-03 19:17:24 -0400

    add perms. for update/delete, kept print for notes

---
+++ thewall/wall_api/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework import permissions
+from django.contrib.auth.models import User
+
+class IsUpdateProfile(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        # can write custom code
+        print('shows article id number:')
+        print(view.kwargs) # shows article id number
+        print('shows current user/logged in')
+        print(request.user) # shows current username
+        print('shows user of message')
+        print(obj.user)
+        print(request.user == obj.user)
+        if request.user == obj.user:
+            return True
+        return False
