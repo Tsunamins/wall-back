0d92f1455d99c24ed8b8ce4ef135200e46b4b1fd
Alison Reilly <reillyamr@gmail.com>
2020-10-03 20:16:58 -0400

    comment out prints, keeping for reference

--- thewall/wall_api/permissions.py
+++ thewall/wall_api/permissions.py
@@ -5,13 +5,13 @@ class UserLoggedInandMatch(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         # kept prints for note to self purposes
-        print('shows article id number:')
-        print(view.kwargs)
-        print('shows current user/logged in')
-        print(request.user)
-        print('shows user of message')
-        print(obj.user)
-        print(request.user == obj.user)
+        # print('shows article id number:')
+        # print(view.kwargs)
+        # print('shows current user/logged in')
+        # print(request.user)
+        # print('shows user of message')
+        # print(obj.user)
+        # print(request.user == obj.user)
         if request.user == obj.user:
             return True
         return False
